15070c43cf9f193ab533adf0ed22a63992ab513e
AKP31 <85797907+AKP31@users.noreply.github.com>
2024-08-29 17:30:36 +0000

    conversion done

---
+++ JS_01/03_conversionOperation.js
@@ -0,0 +1,72 @@
+// let score = true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// //if valueInNumber = 43abc then output is NaN
+// // If it is number(eg. 43) then output is 43
+// // In case , valueInNumber = null then output is 0
+// // In case , valueInNumber = undefined then o/p is NaN
+// // In case , valueInNumber = true/false then o/p is 1/0
+
+
+// let isLog = 1
+
+// let isLogBool = Boolean(isLog)
+// console.log(isLogBool);
+// // 1 =>true
+// // 0 =>false
+// // "" =>false
+// // "Ankit" =>true
+
+// let num = 33;
+// let numString = String(num);
+// console.log(numString); //33
+// console.log(typeof numString); //string
+
+// ----------operations---------
+
+let val = 6
+let negVal = -val
+console.log(negVal);//-6
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);//power
+// console.log(2/8);
+// console.log(2%4);
+
+let str1 = "Hello"
+let str2 = " Ankit"
+
+let str3 = str1 + str2
+console.log(str3);//Hello Ankit
+
+console.log("1" + 2); //12
+console.log(1 + "2"); //12
+console.log("1" + 2 +2); //122
+console.log(1 + 2 + "2"); //32
+
+console.log(true);//true
+console.log("");//blank
+
+console.log(+true); //1
+console.log(+""); //0
+
+let counter = 120;
+++counter;//prefix
+counter++;//postfix
+console.log(counter);
+
+
+
+
+
+
+
+
+
