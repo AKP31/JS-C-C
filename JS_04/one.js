a3d6b9b947e04c3ae398ab6ed2f32df6e82fbb57
AKP31 <85797907+AKP31@users.noreply.github.com>
2024-09-04 14:33:44 +0000

    control statement

---
+++ JS_04/one.js
@@ -0,0 +1,16 @@
+//Strict checking.(===,!==). It checks data type also
+
+if(2==='2'){
+    console.log(executed);
+}
+//short hand syntax.Here scope is implicit
+if(2==='2') console.log(executed);
+
+if(2==='2'){
+    console.log("Type equal");
+}
+else{
+    console.log("Type not equal");
+    
+}
+//&&,||
